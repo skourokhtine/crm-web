201f7c46d175ce747663d8f7aaf0f9fe1ca22b17
Serguei Kourokhtine <skourokhtine@gmail.com>
2015-06-16 16:25:36 -0400

    Clas is added to crm file

--- crm.rb
+++ crm.rb
@@ -1,7 +1,24 @@
 #my way...
-
 require 'sinatra'
 
-get '/' do 
+get '/' do  
+	@crm_app_name = "My CRM"
 	erb :index
 end
+# '/' # This block of code is route. It allows me to match 
+# a particular request method in order to get a specific
+#response for this request.
+
+get '/contacts' do
+	erb :contacts
+end
+
+get '/contacts/new' do
+	erb :contacts_new
+end
+
+
+
+
+
+
