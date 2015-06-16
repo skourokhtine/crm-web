0fc99a8eb1b53162f682d97cdd4aee1191e1dab5
Serguei Kourokhtine <skourokhtine@gmail.com>
2015-06-16 17:53:58 -0400

    Incorporating add new contacts

--- crm.rb
+++ crm.rb
@@ -1,6 +1,8 @@
-require_relative 'rolodex.rb'
+require_relative 'rolodex'
 require_relative 'contact'
-require 'sinatra' #my way...
+require 'sinatra' 
+
+$rolodex= Rolodex.new
 
 get '/' do  
 	@crm_app_name = "My CRM"
@@ -10,22 +12,20 @@ end
 # a particular request method in order to get a specific
 #response for this request.
 
-# get '/contacts' do
-# 	erb :contacts
-# end
-
 get '/contacts/new' do
 	erb :contacts_new
 end
 
 get "/contacts" do
-  @contacts = []
-  @contacts << Contact.new(1000, "Serguei", "Kourokhtine", "skourokhtine@gmail.com", "Bitmaker")
-  @contacts << Contact.new(1001,"Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-  @contacts << Contact.new(1002, "Sergey", "Brin", "sergey@google.com", "Co-Founder")
+  # @contacts = []
+  # @contacts << Contact.new(1000, "Serguei", "Kourokhtine", "skourokhtine@gmail.com", "Bitmaker")
+  # @contacts << Contact.new(1001,"Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+  # @contacts << Contact.new(1002, "Sergey", "Brin", "sergey@google.com", "Co-Founder")
 
   erb :contacts
 end
+
+
 
 
 
